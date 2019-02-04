2ae26b1884f5f4821bd7354985fe253feb979f1d
José Miguel Moreno <josemmo@pm.me>
2019-02-04 12:15:37 +0100

    Added SASS support
    - Updated Webpack configuration
    - Installed SASS dependencies
    - Renamed SCSS entry point

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,20 @@
 /*
- * Welcome to your app's main JavaScript file!
+ * Rosetta - A free (libre) Integrated Library System for the 21st century.
+ * Copyright (C) 2019 José M. Moreno <josemmo@pm.me>
  *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// Load dependencies
+import '../css/app.scss'
