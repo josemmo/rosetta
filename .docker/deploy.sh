475e5e3f7d9fd866f26947184798cd604eef3aad
José Miguel Moreno <josemmo@pm.me>
2019-03-04 13:12:39 +0100

    New deploy system (#23)
    Migrated from Heroku to custom server deploy

---
+++ .docker/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+eval "$(ssh-agent -s)"
+echo "$GIT_DEPLOY_KEY" > deploy_key.pem
+chmod 600 deploy_key.pem
+ssh-add deploy_key.pem
+git remote add deploy $GIT_DEPLOY_URL
+git push deploy
