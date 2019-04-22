17bda19ad8f307e06d30d8336b620dbee1033894
José Miguel Moreno <josemmo@pm.me>
2019-04-22 12:25:00 +0200

    Initial map service (#10)
    - Created Maps Service
    - Created directory for storing maps

---
+++ src/RosettaBundle/Service/MapsService.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Rosetta - A free (libre) Integrated Library System for the 21st century.
+ * Copyright (C) 2019 José M. Moreno <josemmo@pm.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace App\RosettaBundle\Service;
+
+use Symfony\Component\Config\ConfigCache;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class MapsService {
+    const MAP_XMLNS = "https://github.com/josemmo/rosetta";
+
+    private $mapsDir;
+    private $indexPath;
+    private $cache;
+
+    public function __construct(KernelInterface $kernel) {
+        $this->mapsDir = $kernel->getProjectDir() . "/assets/custom/maps";
+        $this->indexPath = $kernel->getCacheDir() . "/maps_index.php.cache";
+        $this->cache = new ConfigCache($this->indexPath, true);
+    }
+
+
+    /**
+     * Get maps index
+     *
+     * The maps index is a file contaning a summary of all data inside the maps directory. It summarizes into a single
+     * file all location data to improve performance when trying to return map data for a particular holding.
+     * This file is generated when new changed are made inside the maps directory.
+     */
+    private function getIndex() {
+        if ($this->cache->isFresh()) return require $this->indexPath;
+        $resources = [];
+        $indexData = [];
+
+        // List all maps in directory
+        $mapNames = array_filter(scandir($this->mapsDir), function($elem) {
+            return (substr($elem, -4) === ".svg");
+        });
+
+        // Parse map files
+        foreach ($mapNames as $mapName) {
+            $mapPath = $this->mapsDir . "/$mapName";
+            $mapData = simplexml_load_file($mapPath);
+            if (empty($mapData)) throw new Exception("Map '$mapName' is not a valid SVG file");
+
+            // Add file to resources to watch
+            $resources[] = new FileResource($mapPath);
+
+            // Find namespace
+            $ns = null;
+            foreach ($mapData->getNamespaces() as $namespace=>$value) {
+                if ($value === self::MAP_XMLNS) {
+                    $ns = $namespace;
+                    break;
+                }
+            }
+            if (is_null($ns)) {
+                throw new Exception("Missing namespace '" . self::MAP_XMLNS . "' from map '$mapName'");
+            }
+
+            // Get map attributes
+            $attributes = $mapData->attributes($ns, true);
+            if (empty($attributes['database'])) {
+                throw new Exception("Missing '$ns:database' from map '$mapName'");
+            }
+            $database = strval($attributes['database']);
+            $locationPattern = empty($attributes['locationPattern']) ? null : strval($attributes['locationPattern']);
+
+            // Find all UDC codes appearing in this map
+            $codes = $this->getUDCs($mapData, $namespace);
+            $codes = array_unique($codes);
+
+            // Add map to index
+            if (!isset($indexData[$database])) $indexData[$database] = [];
+            if (!isset($indexData[$database][$locationPattern])) $indexData[$database][$locationPattern] = [];
+            foreach ($codes as $code) $indexData[$database][$locationPattern][$code] = $mapName;
+        }
+
+        // Cache index
+        $this->cache->write('<?php return ' . var_export($indexData, true) . ';', $resources);
+        return $indexData;
+    }
+
+
+    /**
+     * Get UDC (Universal Decimal Classification) codes
+     * @param  \SimpleXMLElement $xmlElement XML Element
+     * @param  string            $ns         Rosetta namespace
+     * @return string[]                      UDC codes
+     */
+    private function getUDCs(\SimpleXMLElement $xmlElement, string $ns) {
+        $res = [];
+        foreach ($xmlElement->children() as $child) {
+            foreach ($this->getUDCs($child, $ns) as $code) $res[] = $code;
+            $udc = $child->attributes($ns, true)->udc;
+            if (empty($udc)) continue;
+
+            // Parse UDC codes
+            $codes = explode(',', $udc);
+            foreach ($codes as $code) $res[] = trim($code);
+        }
+        return $res;
+    }
+
+}
